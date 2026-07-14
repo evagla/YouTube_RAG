f6e6714f6fc31a84e763f5bba664ef3da10395a6
eva <eva.glasmastar@gmail.com>
2026-07-14 15:00:26 +0200

    chore(cleanup): remove archive folder and outdated tests

--- archive/test_youtube.py
+++
@@ -1,9 +0,0 @@
-from app.ingestion.youtube import fetch_transcript
-
-video_id = "hXlSicZE9jI"  # byt till valfri video
-text = fetch_transcript(video_id)
-
-print(text[:500])  # skriv ut första 500 tecken
-
-
-1 + 1
